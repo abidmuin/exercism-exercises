dfde5a26f716d41733dfb2437bd0a81fe9e9bd0b
abidmuin <muin.739@gmail.com>
2022-11-11 15:48:51 +0600

    Raindrops

---
+++ Bash/raindrops.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# https://exercism.org/tracks/bash/exercises/raindrops/
+
+result=""
+
+main()
+{
+
+	# https://www.gnu.org/savannah-checkouts/gnu/bash/manual/bash.html#Conditional-Constructs
+
+    (($1%3)) || result+="Pling"
+    (($1%5)) || result+="Plang"
+    (($1%7)) || result+="Plong"
+
+    echo ${result:-$1}
+}
+
+main "$@"
+
+echo $((9%2))
