e5f6940397b2380c09c1adcb0376fb0b8bf665f8
abidmuin <muin.739@gmail.com>
2022-11-13 15:17:50 +0600

    Raindrops

--- Bash/raindrops.sh
+++ Bash/raindrops.sh
@@ -9,11 +9,11 @@ main()
 
 	# https://www.gnu.org/savannah-checkouts/gnu/bash/manual/bash.html#Conditional-Constructs
 
-    (($1%3)) || result+="Pling"
-    (($1%5)) || result+="Plang"
-    (($1%7)) || result+="Plong"
+    (( $1%3 )) || result+="Pling"
+    (( $1%5 )) || result+="Plang"
+    (( $1%7 )) || result+="Plong"
 
-    echo ${result:-$1}
+    echo "${result:-$1}"
 }
 
 main "$@"
